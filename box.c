933dbbfcad7b2cacd9e17581a90ccabf4b01567e
99003769 <78853972+99003769@users.noreply.github.com>
2021-02-22 11:34:09 +0530

    Create box.c

---
+++ box.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Boxes {
+    int ID;
+    float length, beradth, height, weight;
+    char color[30];
+};
+
+int main() 
+{
+    struct Boxes *ptr;
+    float volume=0.0, average_volume;
+    int Number, No_of_boxes, choice,i;
+    printf("Enter the number of Boxes\n ");
+    scanf("%d", &No_of_boxes);
+
+    // Memory allocation for Boxes according to user
+    ptr = (struct Boxes *)malloc(No_of_boxes * sizeof(struct Boxes));
+    for (Number = 0; Number < No_of_boxes; ++Number) 
+    {
+        printf("Enter ID, lengthg, breadth, height, weight,and color:\n");
+        scanf("%d %f %f %f %f %s", &(ptr + Number)->ID, &(ptr + Number)->length, &(ptr + Number)->beradth, &(ptr + Number)->height, &(ptr + Number)->weight, &(ptr + Number)->color );
+
+    }
+  //  printf("Displaying Information:\n");
+  //    for (Number = 0; Number < No_of_boxes; ++Number)
+  //    {
+  //         printf("%d\n %f\n %f\n %f\n %f\n %s", (ptr + Number)->ID, (ptr + Number)->length, (ptr + Number)->beradth, (ptr + Number)->height, (ptr + Number)->weight, (ptr + Number)->color );
+  //      }
+        printf(" select \n1.To find average volume of Boxes\n2.Differnce between min and max volume\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            for(i=0;i<No_of_boxes;++i)
+              {
+                volume += (ptr +i)->length*(ptr + i)->beradth*(ptr +i)->height;
+              }
+              average_volume=volume / No_of_boxes;
+             printf("%f",average_volume);
+             break;
+
+             case 2:
+             for(i=0;i<No_of_boxes;++i)
+             {
+
+             }
+        }   
+    
+}
