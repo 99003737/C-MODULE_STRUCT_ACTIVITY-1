4f1d34b23562fd9e369f23314d7ffaeda5ae437f
99003769 <Ankit.raj@ltts.com>
2021-02-22 18:32:40 +0530

    i updated diffrence , min,max,volumes

--- box.c
+++ box.c
@@ -10,11 +10,12 @@ struct Boxes {
 int main() 
 {
     struct Boxes *ptr;
-    float volume=0.0, average_volume;
+    float volume=0.0, average_volume,difference;
     int Number, No_of_boxes, i;
+    int min=0,max=0;
     printf("Enter the number of Boxes\n ");
     scanf("%d", &No_of_boxes);
-
+    int arr[No_of_boxes],box_average=0;
     // Memory allocation for Boxes according to user
     ptr = (struct Boxes *)malloc(No_of_boxes * sizeof(struct Boxes));
     for (Number = 0; Number < No_of_boxes; ++Number) 
@@ -30,10 +31,26 @@ int main()
   //      }
     for(i=0;i<No_of_boxes;++i)
      {
-        volume += (ptr +i)->length*(ptr + i)->beradth*(ptr +i)->height;
+        volume = (ptr +i)->length*(ptr + i)->beradth*(ptr +i)->height;
+        arr[i]=volume;
+        printf("%d\n",arr[i]);
+        box_average+=volume;
      }
-    average_volume=volume / No_of_boxes;
-    printf("%f",average_volume);
+    average_volume=box_average / No_of_boxes;
+    printf("%f\n",average_volume);
+    for(i=0;i<No_of_boxes;i++)
+    {
+        min=max=arr[0];
+        if(arr[i]<min)
+        {
+            min=arr[i];
+        }
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+        difference=max-min;
+        printf("%f",difference);
     return 0;
 }
-
