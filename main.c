a377001f214cf2ad47c1110223b0b0536f12c16b
99003776 <78869826+99003776@users.noreply.github.com>
2021-02-23 09:54:16 +0530

    uploaded the code

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct boxes
+{
+
+    int id;
+    float length, beradth, height, weight;
+    char color[30];
+};
+
+int main()
+{
+    struct boxes *ptr;
+    char *pt;
+    float volume =0.0 , average_volume;
+    int number ,no_of_boxes,choice,i,chars;
+    printf("enter the number of boxes\n");
+    scanf("%d",&no_of_boxes);
+    ptr =(struct boxes *)malloc(no_of_boxes * sizeof(struct boxes));
+
+    //printf("total number of boxes is %d\n", no_of_boxes);
+
+    pt=(char *)malloc((chars+1)*sizeof(char));
+
+    for(i=0;i<no_of_boxes;i++)
+    {
+        printf("enter the box id that you want to find\n");
+        scanf("%s", pt);
+        printf("box id that you have enter is %s\n", pt);
+
+
+    }
+
+    /*for(number=0;number<no_of_boxes;number++)
+    {
+        printf("numbr of boxes %d")
+    }*/
+
+
+
+}
