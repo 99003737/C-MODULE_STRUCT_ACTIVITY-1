1f77a22b7852fae9cdd6a6c47b7eb07d2832206a
99003769 <78853972+99003769@users.noreply.github.com>
2021-02-25 14:45:52 +0530

    Create volume.h

---
+++ Inc/volume.h
@@ -0,0 +1,12 @@
+#ifndef __CALCVOLUME_H
+#define __CALCVOLUME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long volume_of_box(Boxes *box1);
+unsigned long avg_volume(Boxes *box1, unsigned int number_of_boxes);
+
+
+
+#endif
