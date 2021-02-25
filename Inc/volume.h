e3e6f54fa8991317bca46914b1f8bcf2dd414bb9
99003769 <78853972+99003769@users.noreply.github.com>
2021-02-25 14:47:22 +0530

    Update volume.h

--- Inc/volume.h
+++ Inc/volume.h
@@ -1,11 +1,11 @@
-#ifndef __CALCVOLUME_H
-#define __CALCVOLUME_H
+#ifndef __VOLUME_H
+#define __VOLUME_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned long volume_of_box(Boxes *box1);
-unsigned long avg_volume(Boxes *box1, unsigned int number_of_boxes);
+long volume(Boxes *box1);
+long average_volume(Boxes *box1, int number_of_boxes);
 
 
 
